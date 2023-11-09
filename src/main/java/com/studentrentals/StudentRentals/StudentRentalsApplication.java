863c815dbca1c85c3692fc402a6d7732191e6487
Carlo R. Garcia <garciaram.cal@gmail.com>
2023-11-10 00:35:48 +0800

    Creating Java Springboot File

---
+++ src/main/java/com/studentrentals/StudentRentals/StudentRentalsApplication.java
@@ -0,0 +1,13 @@
+package com.studentrentals.StudentRentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentrentals/StudentRentals/StudentRentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentrentals.StudentRentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentRentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
